9afbdf2d4d79db11d4c41de7bfa3abe283bd5047
Zoey Kariyal <zoey.kariyal@stu.ocsb.ca>
2025-05-01 02:58:32 +0000

    temperature checker

--- js/script.js
+++ js/script.js
@@ -3,3 +3,15 @@
 // Created by: Mr. Coxall
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+function checkTemperature() {
+    var celsius = parseFloat(document.getElementById("celsiusInput").value);
+    var result = document.getElementById("result");
+
+    if (isNaN(celsius)) {
+        result.innerText = "Please enter a valid number.";
+    } else if (celsius < 15) {
+        result.innerText = "It is cold outside.";
+    } else {
+        result.innerText = "It is hot outside.";
+    }
+}
